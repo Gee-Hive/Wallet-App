7c6b015d1b5dd0b3343489196ead4268a576d49d
Gbenga Gasky <gbenga@getwallets.co>
2023-01-11 04:10:19 +0100

    initialise server.js file and implement a server side to connect to database

---
+++ server.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./config.env" });
+const app = require("./app");
+
+const DB = process.env.DATABASE.replace(
+  "<password>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB connection established");
+  });
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+  console.log("listening on port " + port);
+});
